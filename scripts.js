173e0fe3bd4ef9fa4eed76613adbf541d02c4f97
SalonZaki <166635183+SalonZaki@users.noreply.github.com>
2024-04-10 17:22:24 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll('nav a');
+
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
